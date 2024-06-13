03a721409ae5a3417d7b1be2217686ab8d70072f
Noam Damari <noamdamari5@gmail.com>
2024-06-13 16:15:18 +0300

    Tasks recycler view setup

--- app/src/main/java/com/example/studyplannerapp/data/models/Task.kt
+++ app/src/main/java/com/example/studyplannerapp/data/models/Task.kt
@@ -1,6 +1,8 @@
 package com.example.studyplannerapp.data.models
 
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 
 data class Task(
     var title: String = "Task",
@@ -10,6 +12,13 @@ data class Task(
     }.timeInMillis,
     var type: TaskType = TaskType.OTHER,
     var course: String = "",
+    var progressPercentage: Int = 0,
     var status: TaskStatus = TaskStatus.OPEN,
-    var image: String? = null) {
+    //var image: String? = null)
+    var image: Int? = null) {
+
+    fun getFormattedDeadline() : String {
+        val deadlineFormat = SimpleDateFormat("dd/MM/yy HH:mm" , Locale.getDefault())
+        return deadlineFormat.format(this.deadline)
+    }
 }
